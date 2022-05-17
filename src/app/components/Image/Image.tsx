fc7ffc04dd2a06408ca3446080555ec574da4f89
happyKale <syk767@gmail.com>
2022-05-17 18:41:16 +0900

    Edit: Image 컴포넌트에 margin prop 추가

--- src/app/components/Image/Image.tsx
+++ src/app/components/Image/Image.tsx
@@ -3,9 +3,10 @@ import styled from "styled-components";
 
 function Image(props: {
   src: string;
+  alt: string;
   width?: string;
   height?: string;
-  alt: string;
+  margin?: string;
 }) {
   // prop destruction
   const { src } = props;
@@ -26,9 +27,14 @@ function Image(props: {
   return <StyledImg loading="lazy" {...props} src={src} />;
 }
 
-const StyledImg = styled.img<{ width?: string; height?: string }>`
+const StyledImg = styled.img<{
+  width?: string;
+  height?: string;
+  margin?: string;
+}>`
   ${(props) => (props.width ? `width: ${props.width}` : "")};
   ${(props) => (props.height ? `height: ${props.height}` : "")};
+  ${(props) => (props.margin ? `margin: ${props.margin}` : "")};
 `;
 
 export { Image };
