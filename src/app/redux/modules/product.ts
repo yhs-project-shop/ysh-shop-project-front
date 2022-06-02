c52f3316b871490be3372534368de164912691e6
happyKale <syk767@gmail.com>
2022-06-02 17:39:02 +0900

    Feat: product 모듈에 미들웨어 추가

--- src/app/redux/modules/product.ts
+++ src/app/redux/modules/product.ts
@@ -1,6 +1,8 @@
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
 import { ProductModel } from "../../models/product-model";
+import { Action, Dispatch } from "redux";
+import { productRepository } from "../../repositories";
 
 // 액션
 const LOAD = "LOAD";
@@ -30,6 +32,86 @@ const initialState: { list: Array<ProductModel> } = {
   ],
 };
 
+// 미들웨어
+const addDB = (productInfo: {
+  name: string;
+  brand: string;
+  category: string;
+  price: number;
+  desc: string;
+  img: string;
+}) => {
+  return async (
+    dispatch: Dispatch<Action>,
+    getState: ProductModel & boolean,
+    { history }: any
+  ) => {
+    const info = {
+      price: productInfo.price,
+      brand: productInfo.brand,
+      category: productInfo.category,
+      name: productInfo.name,
+      image: productInfo.img,
+      desciption: productInfo.desc,
+    };
+    try {
+      console.log("여기가 제품 등록 미들웨어 axios 호출 바로 전!!", info);
+      const response = await productRepository.add(info);
+      console.log("제품등록 결과: ", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+const deleteDB = (produceId: number) => {
+  return async (
+    dispatch: Dispatch<Action>,
+    getState: ProductModel & boolean,
+    { history }: any
+  ) => {
+    try {
+      console.log("여기가 제품 삭제 미들웨어 axios 호출 바로 전!");
+      const response = await productRepository.delete(produceId);
+      console.log("제품삭제 결과: ", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+const getAllDB = (category: string) => {
+  return async (
+    dispatch: Dispatch<Action>,
+    getState: ProductModel & boolean,
+    { history }: any
+  ) => {
+    try {
+      console.log("여기가 제품 전체 가져오기 미들웨어 axios 호출 바로 전!");
+      const response = await productRepository.getAll(category);
+      console.log("제품 전체 가져오기 결과: ", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+const getOneDB = (produceId: number) => {
+  return async (
+    dispatch: Dispatch<Action>,
+    getState: ProductModel & boolean,
+    { history }: any
+  ) => {
+    try {
+      console.log("여기가 제품 하나 가져오기 미들웨어 axios 호출 바로 전!");
+      const response = await productRepository.getOne(produceId);
+      console.log("제품 하나 가져오기 결과: ", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
 // 리듀서
 // 실질적으로 store에 들어가 있는 데이터를 변경하는 곳이죠!
 export default handleActions(
@@ -50,4 +132,8 @@ export const productActions = {
   create,
   update,
   remove,
+  addDB,
+  deleteDB,
+  getAllDB,
+  getOneDB,
 };
