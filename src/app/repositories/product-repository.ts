999c498bb3d6a57b21e8ee5cd1b0c7291323d767
happyKale <syk767@gmail.com>
2022-06-02 17:38:30 +0900

    Feat: product 레포지토리 메소드 추가

--- src/app/repositories/product-repository.ts
+++ src/app/repositories/product-repository.ts
@@ -1,6 +1,26 @@
-import { UserModel } from "../models/user-model";
 import { httpClient } from "../libs/http-client";
 
-const productRepository = {};
+const productRepository = {
+  add(data: {}) {
+    return httpClient.post("/products", data).then((res) => {
+      return res;
+    });
+  },
+  delete(productId: number) {
+    return httpClient.delete(`/products?productId=${productId}`).then((res) => {
+      return res;
+    });
+  },
+  getAll(category: string) {
+    return httpClient.get(`/products/${category}`).then((res) => {
+      return res;
+    });
+  },
+  getOne(productId: number) {
+    return httpClient.get(`/product/${productId}`).then((res) => {
+      return res;
+    });
+  },
+};
 
 export { productRepository };
