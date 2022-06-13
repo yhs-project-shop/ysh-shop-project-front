d313e36e6955e5ad8a0eb01fd5151d6d5784b756
happyKale <syk767@gmail.com>
2022-06-13 19:30:42 +0900

    Feat: 메인화면 DetailButton, DetailText 만들어서 중복되는 버튼, 글자를 줄임

--- src/app/screens/Home/HomeScreen.tsx
+++ src/app/screens/Home/HomeScreen.tsx
@@ -1,17 +1,43 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import styled from "styled-components";
-import { Image } from "../../components";
-import { productActions } from "../../redux/modules/product";
+import { Button, FlexBox, Image, Text } from "../../components";
+import video from "../../../assets/main.png";
 import main from "../../../assets/main_main.webp";
 import ready from "../../../assets/main_ready.webp";
 import youth from "../../../assets/main_youth.webp";
+
+const DetailButton = () => {
+  return (
+    <Button
+      type="button"
+      backgroundColor="#7001f7"
+      width="170px"
+      height="43px"
+      fontSize="18px"
+      radius="21.5px"
+      margin="16px 0 0 0"
+      padding="9px auto 8px"
+    >
+      자세히 보기
+    </Button>
+  );
+};
+
+const DetailText = () => {
+  return (
+    <>
+      <Text size="12px">우리가 원하는 것.</Text>
+      <Text size="12px">
+        자유로움 너머로 나아가는 당신의 여정을 텐드가 함께합니다.
+      </Text>
+    </>
+  );
+};
 
 function HomeScreen() {
   // prop destruction
 
   // lib hooks
-  const dispatch = useDispatch();
 
   // state, ref, querystring hooks
 
@@ -22,25 +48,53 @@ function HomeScreen() {
   // calculated values
 
   // effects
-  useEffect(() => {
-    dispatch(productActions.getAllDB());
-    dispatch(productActions.getOneDB(14));
-  }, []);
 
   // handlers
 
   return (
     <StyledDiv>
+      <Image
+        src={video}
+        alt="Tend 신발 메인 동영상"
+        width="100%"
+        margin="0 0 100px 0"
+      />
       <Image src={main} alt="Tend 신발 메인 이미지" width="100%" />
+      <FlexBox
+        padding="20px 0 0 0"
+        height="160px"
+        direction="column"
+        alignItems="center"
+        boxSizing="border-box"
+      >
+        <DetailText />
+        <DetailButton />
+      </FlexBox>
       <Content1>
-        <Item />
-        <Item />
+        <Image
+          src={ready}
+          alt="메인 이미지_준비자세를 취한 사람"
+          width="592px"
+          margin="0 auto 0 0"
+        />
+        <Image
+          src={youth}
+          alt="메인 이미지_청년들"
+          width="592px"
+          margin="0 0 0 auto"
+        />
       </Content1>
-      <Content2>
-        <Item />
-        <Item />
-        <Item />
-      </Content2>
+      <FlexBox
+        padding="20px 0 0 0"
+        height="160px"
+        direction="column"
+        alignItems="center"
+        boxSizing="border-box"
+      >
+        <DetailText />
+        <DetailButton />
+      </FlexBox>
+      <Content2></Content2>
     </StyledDiv>
   );
 }
@@ -51,18 +105,14 @@ const StyledDiv = styled.div`
   height: auto;
 `;
 
-const Item = styled.div`
-  border: 2px solid black;
-`;
-
 const Content = styled.div`
   display: grid;
-  grid-template-rows: repeat(auto, 1fr);
   grid-auto-flow: dense;
   place-items: stretch stretch;
+  box-sizing: border-box;
 `;
 const Content1 = styled(Content)`
-  height: 680px;
+  height: 592px;
   grid-template-columns: repeat(auto-fill, minmax(50%, auto));
 `;
 const Content2 = styled(Content)`
