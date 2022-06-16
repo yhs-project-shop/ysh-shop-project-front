24bfaa01be1037e5133de0b6d9317c5a90871775
happyKale <syk767@gmail.com>
2022-06-16 21:05:59 +0900

    Edit: Image 컴포넌트 cursor props 추가

--- src/app/components/Image/Image.tsx
+++ src/app/components/Image/Image.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-function Image(props: {
+type ImageProps = {
   src: string;
   alt: string;
   width?: string;
   height?: string;
   margin?: string;
-}) {
+  cursor?: string;
+};
+
+function Image(props: ImageProps) {
   // prop destruction
   const { src } = props;
 
@@ -27,11 +30,8 @@ function Image(props: {
   return <StyledImg loading="lazy" {...props} src={src} />;
 }
 
-const StyledImg = styled.img<{
-  width?: string;
-  height?: string;
-  margin?: string;
-}>`
+const StyledImg = styled.img<ImageProps>`
+  ${(props) => (props.cursor ? `cursor: ${props.cursor}` : "")};
   ${(props) => (props.width ? `width: ${props.width}` : "")};
   ${(props) => (props.height ? `height: ${props.height}` : "")};
   ${(props) => (props.margin ? `margin: ${props.margin}` : "")};
