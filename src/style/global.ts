98801823d0d4f886ba6caa48e3be99e9c85eb1aa
happyKale <syk767@gmail.com>
2022-06-20 11:16:31 +0900

    Edit: index.html의 style 속성 global 스타일로 이동"

--- src/style/global.ts
+++ src/style/global.ts
@@ -3,7 +3,8 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
-        padding: 0;  
+        padding: 0; 
+        font-family: 'Poppins', sans-serif
     }
 
     #content{
