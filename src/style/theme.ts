12a1c61c08faba34c59e93ada3fcda2b9e6ab5f2
happyKale <syk767@gmail.com>
2022-06-20 10:35:11 +0900

    Edit: theme.ts 파일 수정

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,16 +1,20 @@
 const size = {
-    mobile: '600px',
-    tablet: '900px',
-    laptop: '1200px',
-    desktop: '1800px',
-}
-  
-const theme = {
-    mainColor: '#0a4297',
-    mobile: `(max-width: ${size.mobile})`,
-    tablet: `(max-width: ${size.tablet})`,
-    laptop: `(max-width: ${size.laptop})`,
-    desktop: `(min-width: ${size.desktop})`,
-}
-  
-export default theme;
+  mobile: "600px",
+  tablet: "900px",
+  laptop: "1200px",
+  desktop: "1800px",
+};
+
+const color = {
+  purple: "#7001f7",
+};
+
+export const theme = {
+  mainColor: color.purple,
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
+
+export type Theme = typeof theme;
