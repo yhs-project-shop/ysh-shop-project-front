9030fa1494225641b45204f5015bd87ada4a2808
happyKale <syk767@gmail.com>
2022-06-21 22:21:37 +0900

    Edit: ProductBox 컴포넌트 수정

--- src/app/components/ProductBox/ProductBox.tsx
+++ src/app/components/ProductBox/ProductBox.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { FlexBox, Image, Text } from "../../components";
 
-function ProductBox() {
+type ProductBoxProps = {
+  image: string;
+  alt: string;
+  name: string;
+  price: string;
+};
+
+function ProductBox(props: ProductBoxProps) {
   // prop destruction
-
+  const { image, alt, name, price } = { ...props };
   // lib hooks
 
   // state, ref, querystring hooks
@@ -16,7 +24,22 @@ function ProductBox() {
   // effects
 
   // handlers
-  return <div></div>;
+  return (
+    <FlexBox direction="column">
+      <Image src={image} alt={alt} width="390px" cursor="pointer" />
+      <Text size="25px" weight="600" height="35px" margin="20px 0 0 0">
+        {name}
+      </Text>
+      <Text
+        size="18px"
+        color="rgba(0, 0, 0, 0.5)"
+        height="25px"
+        margin="16px 0 0 0"
+      >
+        {price}
+      </Text>
+    </FlexBox>
+  );
 }
 
 export { ProductBox };
